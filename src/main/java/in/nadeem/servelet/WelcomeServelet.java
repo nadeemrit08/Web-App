446d5c0a12cb1696f274575965487aee8d5963d2
Nadeem Ahmad <nadeemrit08.com>
2023-05-20 10:44:18 +0700

    1st commit

---
+++ src/main/java/in/nadeem/servelet/WelcomeServelet.java
@@ -0,0 +1,27 @@
+package in.nadeem.servelet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class WelcomeServelet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		PrintWriter writer = resp.getWriter();
+		writer.write("<h1> Welcome to web Application</h1>");
+		writer.close();
+	}
+}
